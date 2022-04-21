e7734cf1a4e820e39a40e93172d09a1f2d9870bd
Giovani Gomes <giovanigomes2002@gmail.com>
2022-04-20 23:09:03 -0300

    Exercício 07
    Resolução do Exercício 07 - Java 2 e também consertando um erro de digitação.

--- Exercício 07/Maior_menor.java
+++ Exercício 07/Maior_menor.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int Num1,Num2,Num3;
+        int Num1,Num2,Num3, Maior;
 
         Scanner scan = new Scanner(System.in);
         System.out.print("Digite Primeiro do Número: ");
@@ -28,7 +28,20 @@ public class Main {
             System.out.println("O Número maior é " + Num3);
         }
         else {
-            System.out.println("Números são iguais ");
+            System.out.println("Números são iguais - Não tem números Maiores ");
+        }
+        
+        if (Num1 < Num2 && Num1 < Num3 ){
+            System.out.println("O Número menor é " + Num1 );
+        }
+        else if (Num2 < Num1 && Num2 < Num3 ) {
+            System.out.println("O Número menor é " + Num2 );
+        }
+        else if (Num3 < Num2 && Num3 < Num1 ) {
+            System.out.println("O Número menor é " + Num3 );
+        }
+        else {
+            System.out.println("Números são iguais - Não tem números menores ");
         }
 
 
