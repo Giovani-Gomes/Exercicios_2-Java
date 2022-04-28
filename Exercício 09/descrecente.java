33a5fe3b33629e1bdf328d0757c4102a06ab128c
Giovani Gomes <giovanigomes2002@gmail.com>
2022-04-27 21:21:00 -0300

    Exercício 09
    Resolução do Exercício 09 - Java LIsta 2.

---
+++ Exercício 09/descrecente.java
@@ -0,0 +1,50 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int num1,num2,num3, Maior;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Digite Primeiro do Número: ");
+        num1= scan.nextInt();
+
+        System.out.print("Digite Segundo do Número: ");
+        num2= scan.nextInt();
+
+        System.out.print("Digite Terceiro do Número: ");
+        num3= scan.nextInt();
+
+
+        if(num1 < num2 && num1 < num3){
+            if (num2 > num3){
+                System.out.println(num2+ " "+ num3 + "" +  num1);
+            }
+            else {
+                System.out.println(num3 + " " + num2 + " " + num1);
+            }
+        } else if (num2 < num1 && num2 < num3){
+            if (num1 > num3){
+                System.out.println(num1 +" "+ num3 +" "+num2);
+            }
+            else{
+                System.out.println(num3 +" "+ num1 + " " + num2);
+            }
+
+        } else if (num3 < num1 && num3 < num2) {
+            if (num1 > num2){
+                System.out.println(num1 + " "+ num2 + " "+ num3);
+            }
+            else{
+                System.out.println(num2 + " "+ num1 + " "+ num3);
+            }
+        }
+        else {
+            System.out.println("Números iguais !! ");
+        }
+
+    }
+}
