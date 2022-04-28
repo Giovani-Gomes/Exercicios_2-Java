777d7c71040ff26f4c62d26bd68c528248e98a77
Giovani Gomes <giovanigomes2002@gmail.com>
2022-04-27 22:23:43 -0300

    Update Exercício 09
    Simplificando a resolução.

--- Exercício 09/descrecente.java
+++ Exercício 09/descrecente.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int num1,num2,num3, Maior;
+        int num1,num2,num3;
 
         Scanner scan = new Scanner(System.in);
         System.out.print("Digite Primeiro do Número: ");
@@ -19,31 +19,30 @@ public class Main {
         num3= scan.nextInt();
 
 
-        if(num1 < num2 && num1 < num3){
-            if (num2 > num3){
-                System.out.println(num2+ " "+ num3 + "" +  num1);
-            }
-            else {
-                System.out.println(num3 + " " + num2 + " " + num1);
-            }
-        } else if (num2 < num1 && num2 < num3){
-            if (num1 > num3){
-                System.out.println(num1 +" "+ num3 +" "+num2);
-            }
-            else{
-                System.out.println(num3 +" "+ num1 + " " + num2);
-            }
+        if (num1 <= num2 && num1 <= num3 && num2 <= num3){
 
-        } else if (num3 < num1 && num3 < num2) {
-            if (num1 > num2){
-                System.out.println(num1 + " "+ num2 + " "+ num3);
-            }
-            else{
-                System.out.println(num2 + " "+ num1 + " "+ num3);
-            }
-        }
-        else {
-            System.out.println("Números iguais !! ");
+            System.out.println(num3 + " - " + num2 + " - " + num1);
+
+        } else if (num1 <= num2 && num1 <= num3 && num3 <= num2){
+
+            System.out.println(num2 + " - " + num3 + " - " + num1);
+
+        } else if (num2 <= num1 && num2 <= num3 && num1 <= num3){
+
+            System.out.println(num3 + " - " + num1 + " - " + num2);
+
+        } else if (num2 <= num1 && num2 <= num3 && num3 <= num1){
+
+            System.out.println(num1 + " - " + num3 + " - " + num2);
+
+        } else if (num3 <= num1 && num3 <= num2 && num1 <= num2){
+
+            System.out.println(num2 + " - " + num1 + " - " + num3);
+
+        } else if (num3 <= num1 && num3 <= num2 && num2 <= num1){
+
+            System.out.println(num1 + " - " + num2 + " - " + num3);
+
         }
 
     }
