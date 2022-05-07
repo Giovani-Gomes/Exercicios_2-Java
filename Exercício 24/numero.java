4e71e9a3704701ae4508ec8a135fec9c794ab46f
Giovani Gomes <giovanigomes2002@gmail.com>
2022-05-07 03:35:25 -0300

    Exercício 24
    Colocando o exercício correto.

--- Exercício 24/numero.java
+++ Exercício 24/numero.java
@@ -6,23 +6,112 @@
 
         public static void main(String[] args) {
             Scanner scan = new Scanner(System.in);
+            System.out.print("Digite o primeiro número: ");
+            int num1 = scan.nextInt();
+            System.out.print("Digite o Segundo número: ");
+            int num2= scan.nextInt();
+            System.out.print("Qual operação \n1-Soma"+ "\n2-Multiplicação"+"\n3-Subtração"+ "\n4-Divisão");
+           int operacao = scan.nextInt();
+           int resultado;
+           double resto;
 
-            boolean notaValida = false;
+            switch (operacao) {
+                case 1:
+                    resultado = num1 + num2;
+                     resto =  num1 % num2;
+                    if (resultado % 2 == 0) {
+                        System.out.println("É PAR");
+                    } else {
+                        System.out.println("É IMPAR");
+                    }
 
-            do {
+                    if(resultado > 0){
+                        System.out.println("É Positivo");
+                    }
+                    else{
+                        System.out.println("É Negativo");
+                    }
 
-                System.out.println("Entre com uma nota");
+                    if( resto != 0){
+                        System.out.println("É Decimal");
+                    }
+                    else {
+                        System.out.println("É Inteiro");
+                    }
+                    break;
+                case 2:
+                    resultado = num1 * num2;
+                     resto =  num1 % num2;
+                    if (resultado % 2 == 0) {
+                        System.out.println("É PAR");
+                    } else {
+                        System.out.println("É IMPAR");
+                    }
 
-                double nota = scan.nextDouble();
+                    if(resultado > 0){
+                        System.out.println("É Positivo");
+                    }
+                    else{
+                        System.out.println("É Negativo");
+                    }
 
-                if (nota >=0 && nota <= 10){
-                    notaValida = true;
-                    System.out.println("Você digitou: " + nota);
-                } else {
-                    //notaValida = false;
-                    System.out.println("Nota inválida, digite novamente.");
-                }
+                    if( resto != 0){
+                        System.out.println("É Decimal");
+                    }
+                    else {
+                        System.out.println("É Inteiro");
+                    }
+                    break;
+                case 3:
+                    resultado = num1 - num2;
+                    resto =  num1 % num2;
+                    if (resultado % 2 == 0) {
+                        System.out.println("É PAR");
+                    } else {
+                        System.out.println("É IMPAR");
+                    }
 
-            } while (!notaValida);
+                    if(resultado > 0){
+                        System.out.println("É Positivo");
+                    }
+                    else{
+                        System.out.println("É Negativo");
+                    }
+
+                    if( resto != 0){
+                        System.out.println("É Decimal");
+                    }
+                    else {
+                        System.out.println("É Inteiro");
+                    }
+                    break;
+                case 4 :
+                    resultado = num1 / num2;
+                     resto =  num1 % num2;
+                    if (resultado % 2 == 0) {
+                        System.out.println("É PAR");
+                    } else {
+                        System.out.println("É IMPAR");
+                    }
+
+                    if(resultado > 0){
+                        System.out.println("É Positivo");
+                    }
+                    else{
+                        System.out.println("É Negativo");
+                    }
+
+                    if( resto != 0){
+                        System.out.println("É Decimal");
+                    }
+                    else {
+                        System.out.println("É Inteiro");
+                    }
+                    break;
+                default:
+                    System.out.println("Operação inválida");
+                    break;
+
+            }
         }
     }
